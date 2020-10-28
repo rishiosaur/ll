22bad9220fbc1e6ae28c34b7cb045fcf488b005c
Rishi Kothari <itsrishikothari@gmail.com>
2020-10-28 09:00:57 -0400

    fixes

--- api/redirect.ts
+++ api/redirect.ts
@@ -1,38 +1,43 @@
-import { NowRequest, NowResponse } from '@vercel/node';
-import firebase from 'firebase';
+import { NowRequest, NowResponse } from '@vercel/node'
+import firebase from 'firebase'
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const { id } = req.query;
+	const { id } = req.query
 
-  const {
-    apiKey,
-    authDomain,
-    databaseURL,
-    projectId,
-    storageBucket,
-    messagingSenderId,
-    appId,
-    measurementId,
-  } = process.env;
+	const {
+		apiKey,
+		authDomain,
+		databaseURL,
+		projectId,
+		storageBucket,
+		messagingSenderId,
+		appId,
+		measurementId,
+	} = process.env
 
- const f = firebase.initializeApp({
-    apiKey,
-    authDomain,
-    databaseURL,
-    projectId,
-    storageBucket,
-    messagingSenderId,
-    appId,
-    measurementId,
-  })
+	const f = firebase.initializeApp({
+		apiKey,
+		authDomain,
+		databaseURL,
+		projectId,
+		storageBucket,
+		messagingSenderId,
+		appId,
+		measurementId,
+	})
 
-  const url = (await f.firestore().collection('routes').doc(id).get().then(x => x.data()))
+	const url = await f
+		.firestore()
+		.collection('routes')
+		.doc(id)
+		.get()
+		.then((x) => x.data())
 
-  if(url) {
-      res.redirect(301, url.url)
-  } else {
-      res.redirect(301, 'https://rishi.cx')
-  }
+	if (url) {
+		res.redirect(301, url.url)
+	} else {
+		res.redirect(301, 'https://rishi.cx')
+	}
 
-  res.end('Hello');
-};
+	res.end('Hello')
+}
