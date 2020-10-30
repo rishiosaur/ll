19f9972f76b1cfb02249d385377865a2161a20aa
Rishi Kothari <itsrishikothari@gmail.com>
2020-10-30 08:14:48 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,18 +3,14 @@ const fetch = require('node-fetch')
 module.exports = {
 	async redirects() {
 		const urls = await fetch(
-			`https://firestore.googleapis.com/v1/projects/${process.env.projectId}/databases/(default)/documents/routes/`
+			process.env.apiURL
 		)
 			.then((x) => x.json())
-			.then((x) => x.documents)
 
 		return urls.map((x) => ({
 			permanent: true,
-			source: `/${x.name.replace(
-				`projects/${process.env.projectId}/databases/(default)/documents/routes/`,
-				''
-			)}/:stuff*`,
-			destination: `${x.fields.url.stringValue}/:stuff*`,
+			source: `/${x.name}/:wildcards*`,
+			destination: `${x.url}/:wildcards*`,
 		}))
 	},
 	github: {
