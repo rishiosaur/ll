4ec3dcbeb14c8f516df5fb3e13605de40b1bca09
Rishi Kothari <itsrishikothari@gmail.com>
2020-10-30 12:16:43 -0400

    update conf for baseUrl and list

--- next.config.js
+++ next.config.js
@@ -4,11 +4,42 @@ module.exports = {
 	async redirects() {
 		const urls = await fetch(process.env.apiURL).then((x) => x.json())
 
-		return urls.map((x) => ({
+		const redirects = urls.map((x) => ({
 			permanent: true,
 			source: `/${x.name}/:wildcards*`,
 			destination: `${x.url}/:wildcards*`,
 		}))
+
+		if (process.env.baseUrl) {
+			redirects.push({
+				permanent: true,
+				source: '/',
+				destination: `${process.env.baseUrl}`,
+			})
+		}
+
+		console.log(redirects)
+
+		return redirects
+	},
+	async rewrites() {
+		const rewrites = []
+
+		if (!process.env.baseUrl) {
+			rewrites.push({
+				source: '/',
+				destination: '/list',
+			})
+		}
+
+		if (process.env.list) {
+			rewrites.push({
+				source: process.env.list,
+				destination: '/list',
+			})
+		}
+
+		return rewrites
 	},
 	github: {
 		autoAlias: true,
