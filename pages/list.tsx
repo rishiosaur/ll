129ed3fd19a41df3bc92dfea03b06aca85c0eaa5
Rishi Kothari <itsrishikothari@gmail.com>
2020-10-30 22:52:24 -0400

    update list

--- pages/list.tsx
+++ pages/list.tsx
@@ -8,6 +8,7 @@ import {
 	Text,
 	useMediaQuery,
 	Tooltip,
+	Image,
 } from '@geist-ui/react'
 import { QuestionCircle } from '@geist-ui/react-icons'
 const fetch = require('node-fetch')
@@ -20,6 +21,11 @@ const HomeRoutes = ({ routes }) => {
 	return (
 		<>
 			<Page size="small">
+				{upMD && (
+					<Image src="https://source.unsplash.com/drNRC5YO184/1500x700" />
+				)}
+
+				<Spacer />
 				<Text h1>
 					{' '}
 					Public Links{' '}
@@ -31,8 +37,9 @@ const HomeRoutes = ({ routes }) => {
 				</Text>
 
 				<Text>
-					Welcome to LL! Here are some interesting links that I've publicly
-					shared (URL parameter forwarding <i>is</i> available):
+					Welcome to the centre of Rishi's Internet life! Here are some
+					interesting links that I've publicly shared (URL parameter forwarding{' '}
+					<i>is</i> available):
 				</Text>
 				<Table
 					data={routes.map((route) => ({
